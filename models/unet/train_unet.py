c9f1c90e42e431592cd579a7800eb94fa7d898cd
amritkumar9595 <amritkumar9595@gmail.com>
2019-12-05 13:44:59 +0530

    initial commit

--- models/unet/train_unet.py
+++ models/unet/train_unet.py
@@ -18,9 +18,12 @@ from tensorboardX import SummaryWriter
 from torch.nn import functional as F
 from torch.utils.data import DataLoader
 
+import sys, os
+sys.path.append(os.getcwd())
+
 from common.args import Args
 from common.subsample import MaskFunc
-from data import transforms
+
 from data.mri_data import SliceData
 from models.unet.unet_model import UnetModel
 
@@ -28,69 +31,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-class DataTransform:
-    """
-    Data Transformer for training U-Net models.
-    """
-
-    def __init__(self, mask_func, resolution, which_challenge, use_seed=True):
-        """
-        Args:
-            mask_func (common.subsample.MaskFunc): A function that can create a mask of
-                appropriate shape.
-            resolution (int): Resolution of the image.
-            which_challenge (str): Either "singlecoil" or "multicoil" denoting the dataset.
-            use_seed (bool): If true, this class computes a pseudo random number generator seed
-                from the filename. This ensures that the same mask is used for all the slices of
-                a given volume every time.
-        """
-        if which_challenge not in ('singlecoil', 'multicoil'):
-            raise ValueError(f'Challenge should either be "singlecoil" or "multicoil"')
-        self.mask_func = mask_func
-        self.resolution = resolution
-        self.which_challenge = which_challenge
-        self.use_seed = use_seed
-
-    def __call__(self, kspace, target, attrs, fname, slice):
-        """
-        Args:
-            kspace (numpy.array): Input k-space of shape (num_coils, rows, cols, 2) for multi-coil
-                data or (rows, cols, 2) for single coil data.
-            target (numpy.array): Target image
-            attrs (dict): Acquisition related information stored in the HDF5 object.
-            fname (str): File name
-            slice (int): Serial number of the slice.
-        Returns:
-            (tuple): tuple containing:
-                image (torch.Tensor): Zero-filled input image.
-                target (torch.Tensor): Target image converted to a torch Tensor.
-                mean (float): Mean value used for normalization.
-                std (float): Standard deviation value used for normalization.
-                norm (float): L2 norm of the entire volume.
-        """
-        kspace = transforms.to_tensor(kspace)
-        # Apply mask
-        seed = None if not self.use_seed else tuple(map(ord, fname))
-        masked_kspace, mask = transforms.apply_mask(kspace, self.mask_func, seed)
-        # Inverse Fourier Transform to get zero filled solution
-        image = transforms.ifft2(masked_kspace)
-        # Crop input image
-        image = transforms.complex_center_crop(image, (self.resolution, self.resolution))
-        # Absolute value
-        image = transforms.complex_abs(image)
-        # Apply Root-Sum-of-Squares if multicoil data
-        if self.which_challenge == 'multicoil':
-            image = transforms.root_sum_of_squares(image)
-        # Normalize input
-        image, mean, std = transforms.normalize_instance(image, eps=1e-11)
-        image = image.clamp(-6, 6)
-
-        target = transforms.to_tensor(target)
-        # Normalize target
-        target = transforms.normalize(target, mean, std, eps=1e-11)
-        target = target.clamp(-6, 6)
-        return image, target, mean, std, attrs['norm'].astype(np.float32)
-
+from unet_data import DataTransform
 
 def create_datasets(args):
     train_mask = MaskFunc(args.center_fractions, args.accelerations)
